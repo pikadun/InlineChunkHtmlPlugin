aa17b61dac8e46425d97063735ce33daec7ddb2a
Dunn <wg0121@outlook.com>
2020-05-19 22:25:44 +0800

    fix: d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,10 @@
-declare module 'inline-chunk-html-plugin' {
-    export class InlineChunkHtmlPlugin {
-        private htmlWebpackPlugin;
-        private tests;
-        constructor(htmlWebpackPlugin: any, tests: any);
-        getInlinedTag(publicPath: any, assets: {
-            [x: string]: any;
-        }, tag: {
-            tagName: string;
-            attributes: {
-                src: string;
-            };
-        }): {
-            tagName: string;
-            attributes: {
-                src: string;
-            };
-        } | {
-            tagName: string;
-            innerHTML: any;
-            closeTag: boolean;
-        };
-        apply(compiler: {
-            options: {
-                output: {
-                    publicPath: string;
-                };
-            };
-            hooks: {
-                compilation: {
-                    tap: (arg0: string, arg1: (compilation: any) => void) => void;
-                };
-            };
-        }): void;
-    }
+export = inline_chunk_html_plugin;
+
+declare class inline_chunk_html_plugin {
+    constructor(...args: any[]);
+
+    apply(...args: any[]): void;
+
+    getInlinedTag(...args: any[]): void;
+
 }
